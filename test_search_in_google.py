ebd622c1159fcfb83c8889f4ebf61dc89c4f8b1c
shadowkatja <shadowkatja@gmail.com>
2023-09-20 09:49:55 +0300

    test google_should_not_have was added

--- test_search_in_google.py
+++ test_search_in_google.py
@@ -4,3 +4,8 @@ def test_google_should_have():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+def test_google_should_not_have():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('axkbxabcnocanpc').press_enter()
+    browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
