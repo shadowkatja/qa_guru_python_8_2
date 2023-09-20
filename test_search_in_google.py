4a118c9ecccece8c9826d88ee750d2d3d9fa015d
shadowkatja <shadowkatja@gmail.com>
2023-09-20 09:35:28 +0300

    fixture browser_setting was added

--- test_search_in_google.py
+++ test_search_in_google.py
@@ -1,7 +1,13 @@
 from selene.support.shared import browser
 from selene import be, have
+import pytest
 
-def test_google_should_have():
+@pytest.fixture()
+def browser_settings():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1020
+
+def test_google_should_have(browser_settings):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
